4edb24c9c0b88163248b0dd027f09218eeea4c7b
arafatrahman <arafatrahmank@gmail.com>
2024-11-12 11:27:17 +0000

    color picker filed is not working

--- includes/View/ColorPicker_Widget_View.php
+++ includes/View/ColorPicker_Widget_View.php
@@ -3,6 +3,10 @@ namespace MFPRO\View;
 
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Background;
+use Elementor\Group_Control_Box_Shadow;
 
 if (!defined('ABSPATH')) exit;
 
@@ -32,13 +36,129 @@ class ColorPicker_Widget_View extends Widget_Base {
             ]
         );
 
-        // Add a color picker control
-        $this->add_control(
-            'color_picker',
+           // Label Text Control
+           $this->add_control(
+            'label_text',
             [
-                'label' => __('Choose Color', 'metformpro'),
+                'label' => __('Label Text', 'metformpro'),
+                'type' => Controls_Manager::TEXT,
+                'default' => __('Select Color', 'metformpro'),
+            ]
+        );
+
+        // Show/Hide Label Control
+        $this->add_control(
+            'show_label',
+            [
+                'label' => __('Show Label', 'metformpro'),
+                'type' => Controls_Manager::SWITCHER,
+                'label_on' => __('Show', 'metformpro'),
+                'label_off' => __('Hide', 'metformpro'),
+                'return_value' => 'yes',
+                'default' => 'yes',
+            ]
+        );
+
+        // Label Position Control
+        $this->add_control(
+            'label_position',
+            [
+                'label' => __('Label Position', 'metformpro'),
+                'type' => Controls_Manager::SELECT,
+                'options' => [
+                    'top' => __('Top', 'metformpro'),
+                    'left' => __('Left', 'metformpro'),
+                ],
+                'default' => 'top',
+                'condition' => [
+                    'show_label' => 'yes',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+         // Style section for label
+         $this->start_controls_section(
+            'label_style_section',
+            [
+                'label' => __('Label', 'metformpro'),
+                'tab' => Controls_Manager::TAB_STYLE,
+                'condition' => [
+                    'show_label' => 'yes',
+                ],
+            ]
+        );
+
+        // Label Typography
+        $this->add_group_control(
+            Group_Control_Typography::get_type(),
+            [
+                'name' => 'label_typography',
+                'label' => __('Typography', 'metformpro'),
+                'selector' => '{{WRAPPER}} .country-selector-wrapper label',
+            ]
+        );
+
+        // Label Color
+        $this->add_control(
+            'label_color',
+            [
+                'label' => __('Text Color', 'metformpro'),
                 'type' => Controls_Manager::COLOR,
-                'default' => '#ffffff',
+                'default' => '#333', // Default label color
+                'selectors' => [
+                    '{{WRAPPER}} .country-selector-wrapper label' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        // Label Margin
+        $this->add_responsive_control(
+            'label_margin',
+            [
+                'label' => __('Margin', 'metformpro'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%', 'em'],
+                'selectors' => [
+                    '{{WRAPPER}} .country-selector-wrapper label' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+         // COLOR Border
+         $this->add_group_control(
+            Group_Control_Border::get_type(),
+            [
+                'name' => 'color_border',
+                'label' => __('Border', 'metformpro'),
+                'selector' => '{{WRAPPER}} .country-selector-wrapper select',
+            ]
+        );
+
+        // color Border Radius
+        $this->add_responsive_control(
+            'color_border_radius',
+            [
+                'label' => __('Border Radius', 'metformpro'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%', 'em'],
+                'selectors' => [
+                    '{{WRAPPER}} .country-selector-wrapper select' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        // color Padding
+        $this->add_responsive_control(
+            'color_padding',
+            [
+                'label' => __('Padding', 'metformpro'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%', 'em'],
+                'selectors' => [
+                    '{{WRAPPER}} .country-selector-wrapper select' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
             ]
         );
 
@@ -46,12 +166,18 @@ class ColorPicker_Widget_View extends Widget_Base {
     }
 
     protected function render() {
+      
+
         $settings = $this->get_settings_for_display();
-    
-        // Output an input field with the color picker as its default value
-        echo '<div class="color-picker-input-wrapper">';
-        echo '<label for="mf-color-picker">' . __('Selected Color:', 'metformpro') . '</label>';
-        echo '<input type="color" id="mf-color" name="mf-color" value="' . esc_attr($settings['color_picker']) . '" />';
+       
+        $label_class = $settings['label_position'] === 'left' ? 'label-left' : 'label-top';
+
+        echo "<div class='color-selector-wrapper '{$label_class}>";
+        if ($settings['show_label'] === 'yes') {
+            echo "<label for='mf-color'>{$settings['label_text']}</label>";
+        }
+              
+        echo '<input type="color" id="mf-color" name="mf-color" />';
         echo '</div>';
         
     }
