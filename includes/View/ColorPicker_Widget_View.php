f83899c9ac4c6de12984d087c3782131987dc9f9
arafatrahman <arafatrahmank@gmail.com>
2024-11-12 11:47:22 +0000

    color field label style added

--- includes/View/ColorPicker_Widget_View.php
+++ includes/View/ColorPicker_Widget_View.php
@@ -36,8 +36,8 @@ class ColorPicker_Widget_View extends Widget_Base {
             ]
         );
 
-           // Label Text Control
-           $this->add_control(
+        // Label Text Control
+        $this->add_control(
             'label_text',
             [
                 'label' => __('Label Text', 'metformpro'),
@@ -78,8 +78,8 @@ class ColorPicker_Widget_View extends Widget_Base {
 
         $this->end_controls_section();
 
-         // Style section for label
-         $this->start_controls_section(
+        // Style section for label
+        $this->start_controls_section(
             'label_style_section',
             [
                 'label' => __('Label', 'metformpro'),
@@ -90,30 +90,29 @@ class ColorPicker_Widget_View extends Widget_Base {
             ]
         );
 
-        // Label Typography
+        // Label Typography Control
         $this->add_group_control(
             Group_Control_Typography::get_type(),
             [
                 'name' => 'label_typography',
                 'label' => __('Typography', 'metformpro'),
-                'selector' => '{{WRAPPER}} .country-selector-wrapper label',
+                'selector' => '{{WRAPPER}} .color-selector-wrapper label',
             ]
         );
 
-        // Label Color
+        // Label Text Color Control
         $this->add_control(
             'label_color',
             [
                 'label' => __('Text Color', 'metformpro'),
                 'type' => Controls_Manager::COLOR,
-                'default' => '#333', // Default label color
                 'selectors' => [
-                    '{{WRAPPER}} .country-selector-wrapper label' => 'color: {{VALUE}};',
+                    '{{WRAPPER}} .color-selector-wrapper label' => 'color: {{VALUE}};',
                 ],
             ]
         );
 
-        // Label Margin
+        // Label Margin Control
         $this->add_responsive_control(
             'label_margin',
             [
@@ -121,67 +120,40 @@ class ColorPicker_Widget_View extends Widget_Base {
                 'type' => Controls_Manager::DIMENSIONS,
                 'size_units' => ['px', '%', 'em'],
                 'selectors' => [
-                    '{{WRAPPER}} .country-selector-wrapper label' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                    '{{WRAPPER}} .color-selector-wrapper label' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
                 ],
             ]
         );
 
-         // COLOR Border
-         $this->add_group_control(
-            Group_Control_Border::get_type(),
-            [
-                'name' => 'color_border',
-                'label' => __('Border', 'metformpro'),
-                'selector' => '{{WRAPPER}} .country-selector-wrapper select',
-            ]
-        );
-
-        // color Border Radius
+        // Label Padding Control
         $this->add_responsive_control(
-            'color_border_radius',
-            [
-                'label' => __('Border Radius', 'metformpro'),
-                'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => ['px', '%', 'em'],
-                'selectors' => [
-                    '{{WRAPPER}} .country-selector-wrapper select' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-                ],
-            ]
-        );
-
-        // color Padding
-        $this->add_responsive_control(
-            'color_padding',
+            'label_padding',
             [
                 'label' => __('Padding', 'metformpro'),
                 'type' => Controls_Manager::DIMENSIONS,
                 'size_units' => ['px', '%', 'em'],
                 'selectors' => [
-                    '{{WRAPPER}} .country-selector-wrapper select' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                    '{{WRAPPER}} .color-selector-wrapper label' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
                 ],
             ]
         );
+
 
         $this->end_controls_section();
     }
 
     protected function render() {
-      
-
         $settings = $this->get_settings_for_display();
-       
+
+        // Apply the label position class
         $label_class = $settings['label_position'] === 'left' ? 'label-left' : 'label-top';
 
-        echo "<div class='color-selector-wrapper '{$label_class}>";
+        echo "<div class='color-selector-wrapper {$label_class}'>";
         if ($settings['show_label'] === 'yes') {
-            echo "<label for='mf-color'>{$settings['label_text']}</label>";
+            echo "<label for='mf-color' class='color-picker-label'>{$settings['label_text']}</label>";
         }
               
         echo '<input type="color" id="mf-color" name="mf-color" />';
         echo '</div>';
-        
     }
-
-
-    
 }
